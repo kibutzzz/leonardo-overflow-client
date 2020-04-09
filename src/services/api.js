260edc5ba1114bb904fc8ed492cddac37226823d
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2020-04-09 14:31:20 -0300

    add authorization header on every request

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
+import { getAuthentication } from './auth';
 
 const api = axios.create({
   baseURL: "http://localhost:8080",
   mode: "no-cors"
 })
+
+
+api.interceptors.request.use(async config => {
+  const authentication = getAuthentication();
+  if (authentication) {
+    config.headers.Authorization = `${authentication.type} ${authentication.token}`
+  }
+
+  return config
+});
 
 export default api;
